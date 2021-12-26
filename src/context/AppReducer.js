6da18d5533b30e153820b52de1643647d24333ab
joshfransix <joshiandersonk69@gmail.com>
2021-12-26 22:58:15 +0100

    fixed_error

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+let stateAction = (state, action) => {
   switch (action.type) {
     case "DELETE_TRANSACTION":
       return {
@@ -18,3 +18,5 @@ export default (state, action) => {
       return state;
   }
 };
+
+export default stateAction;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
